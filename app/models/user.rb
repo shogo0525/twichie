39cad1bda02a2fb2e0ba77a622c8de20371f39df
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-01-19 18:26:21 +0900

    Add few methods to Authorization and User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :authorizations
+
+  def self.create_from_auth!(auth)
+    self.create!(
+      email: auth["info"]["email"],
+      nickname: auth["info"]["name"]
+    )
+  end
 end
